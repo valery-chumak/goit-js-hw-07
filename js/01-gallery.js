25328054e10b6fe4ba804546a631acc8511cc76a
valery-chumak <blossssom88@gmail.com>
2022-07-17 12:36:04 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -16,22 +16,33 @@ const galleryItemsMarkup = galleryItems.map(item => `<div class="gallery__item">
 gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
 
 gallery.addEventListener('click', onGalleryClick);
+
 let instance;
+
 function onGalleryClick(event) {
     event.preventDefault();
     if (!event.target.classList.contains("gallery__image")) {
         return;
-    } else {
-        const dataSource = event.target.dataset.source;
-        instance = basicLightbox.create(`<img src="${dataSource}">`);
-        instance.show();
-        window.addEventListener("keydown", onEscapeButtonPress);
-    }
-    function onEscapeButtonPress(event) {
-        if (event.code === 'Escape') {
-            instance.close();
-        }
-    }
+    } 
+      
+    const dataSource = event.target.dataset.source;
+    instance = basicLightbox.create(`<img src="${dataSource}">`, {
+    onShow: instance => {
+      window.addEventListener("keydown", onEscapeButtonPress);
+    },
+    onClose: instance => {
+      window.removeEventListener("keydown", onEscapeButtonPress);
+    },
+    });
+    instance.show();
+
 }
+
+  function onEscapeButtonPress(event) {
+        if (event.code === 'Escape') {
+          instance.close();
+          return;
+        }
+  }
 
 console.log(galleryItems);
